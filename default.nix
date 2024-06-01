ec0e1de31aecbfa8cfbd5e3f96224a8e383cd387
Seth Speaks <sspeaks610@gmail.com>
2024-05-31 21:55:16 -0700

    fix spock build

--- default.nix
+++ default.nix
@@ -1,2 +1,10 @@
 {pkgs,...}:
-pkgs.haskellPackages.callPackage ./spock.nix {}
+let 
+  Spock-core = pkgs.haskell.lib.doJailbreak (pkgs.haskellPackages.Spock-core);
+  hPacks = pkgs.haskellPackages.override {
+    overrides = _: _: {
+      inherit Spock-core;
+    };
+  };
+in
+hPacks.callCabal2nix "spock" ./. {}
