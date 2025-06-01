d33ad0cf150a5c40068fb71654e30066065951fe
Seth Speaks <sspeaks610@gmail.com>
2025-05-31 21:13:33 -0700

    update spock to determinate and 25.05

--- default.nix
+++ default.nix
@@ -1,16 +1,11 @@
-{ pkgs, ... }:
-let
-  spockSource = pkgs.fetchFromGitHub {
-    owner = "agrafix";
-    repo = "Spock";
-    rev = "40d028bfea0e94ca7096c719cd024ca47a46e559";
-    hash = "sha256-HIsVmGa9eOBjIc70asMuYbarv8C5ipxucAKUGltpbpc=";
-  };
-  Spock-core = pkgs.haskellPackages.callCabal2nixWithOptions "Spock" spockSource "--subpath Spock-core" { };
+{pkgs,...}:
+let 
+  Spock-core = pkgs.haskell.lib.doJailbreak (pkgs.haskellPackages.Spock-core);
+  serverSource = builtins.path { path = ./.; name = "source"; };
   hPacks = pkgs.haskellPackages.override {
     overrides = _: _: {
       inherit Spock-core;
     };
   };
 in
-hPacks.callCabal2nix "spock" ./. { }
+hPacks.callCabal2nix "spock" serverSource {}
--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
   };
   outputs = { nixpkgs, ... }:
     let
